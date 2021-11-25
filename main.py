cd286db9495e0727f84270125dd94146f4ff5a5a
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-11-25 17:31:22 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is our posts"}
+
+
+@app.post("/createpost")
+def create_posts(post: Post):
+    return {"data": post}
